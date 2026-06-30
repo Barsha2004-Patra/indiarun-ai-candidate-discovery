0e47be808e2faf38e61872503f4ed6eeb8bc5597
Barsha2004-Patra <pbarsha718@gmail.com>
2026-06-30 17:28:18 +0530

    Clean project structure by removing unused files

--- test_import.py
+++
@@ -1,9 +0,0 @@
-print("Step 1")
-
-from sentence_transformers import SentenceTransformer
-
-print("Step 2")
-
-model = SentenceTransformer("all-MiniLM-L6-v2")
-
-print("Loaded successfully")
--- test_pandas.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-print("Pandas OK")
-
-from sentence_transformers import SentenceTransformer
-
-print("SentenceTransformer OK")
